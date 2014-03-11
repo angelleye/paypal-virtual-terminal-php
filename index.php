43a536cc556b4a656a153c084decf319dde7ec34
Andrew Angell <andrew+git@angelleye.com>
2014-03-11 09:20:37 -0500

    Updates expiration year values

--- index.php
+++ index.php
@@ -135,10 +135,6 @@
           <li>
             <label for="CreditCardExpYear">Exp. Year </label>
             <select name="CreditCardExpYear" id="CreditCardExpYear">
-              <option value="2010">2010</option>
-              <option value="2011">2011</option>
-              <option value="2012">2012</option>
-              <option value="2013">2013</option>
               <option value="2014">2014</option>
               <option value="2015">2015</option>
               <option value="2016">2016</option>
@@ -146,6 +142,11 @@
               <option value="2018">2018</option>
               <option value="2019">2019</option>
               <option value="2020">2020</option>
+              <option value="2021">2021</option>
+              <option value="2022">2022</option>
+              <option value="2023">2023</option>
+              <option value="2024">2024</option>
+              <option value="2025">2025</option>
             </select>
           </li>
           <div id="DivCreditCardIssueNumber">
