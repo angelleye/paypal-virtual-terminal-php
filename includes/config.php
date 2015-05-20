c6a4f56739f89e1159d8a6ceaaf135af324b0715
Andrew Angell <service@angelleye.com>
2015-05-20 09:36:08 -0500

    Adds more instruction to config options

--- includes/config.php
+++ includes/config.php
@@ -4,72 +4,17 @@
  */
 date_default_timezone_set('America/Chicago');
 
+/**
+ * Setup $config array for options to be added later in the config file.
+ */
 $config = array();
 
 /**
  * Sandbox / Test Mode
- * -------------------------
+ * -------------------
  * TRUE means you'll be hitting PayPal's sandbox/test servers.  FALSE means you'll be hitting the live servers.
  */
 $config['Sandbox'] = TRUE;
-
-/**
- * API selection
- * 'PayPalPro-DDP, PayPalPro-PayFlow, PayPal-Rest'
- */
-$config['ApiSelection'] = 'PayPal-Rest';
-
-/*
- * PayPal API Version
- * ------------------
- * The library is currently using PayPal API version 119.0.
- * You may adjust this value here and then pass it into the PayPal object when you create it within your scripts to override if necessary.
- */
-$config['APIVersion'] = '119.0';
-
-/*
- * PayPal Gateway API Credentials
- * ------------------------------
- * These are your PayPal API credentials for working with the PayPal gateway directly.
- * These are used any time you're using the parent PayPal class within the library.
- *
- * We're using shorthand if/else statements here to set both Sandbox and Production values.
- * Your sandbox values go on the left and your live values go on the right.
- *
- * You may obtain these credentials by logging into the following with your PayPal account: https://www.paypal.com/us/cgi-bin/webscr?cmd=_login-api-run
- */
-$config['APIUsername'] = $config['Sandbox'] ? 'sandbo_1215254764_biz_api1.angelleye.com' : 'PRODUCTION_USERNAME_GOGES_HERE';
-$config['APIPassword'] = $config['Sandbox'] ? '1215254774' : 'PRODUCTION_PASSWORD_GOGES_HERE';
-$config['APISignature'] = $config['Sandbox'] ? 'AiKZhEEPLJjSIccz.2M.tbyW5YFwAb6E3l6my.pY9br1z2qxKx96W18v' : 'PRODUCTION_SIGNATURE_GOGES_HERE';
-
-/*
- * Payflow Gateway API Credentials
- * ------------------------------
- * These are the credentials you use for your PayPal Manager:  http://manager.paypal.com
- * These are used when you're working with the PayFlow child class.
- *
- * We're using shorthand if/else statements here to set both Sandbox and Production values.
- * Your sandbox values go on the left and your live values go on the right.
- *
- * You may use the same credentials you use to login to your PayPal Manager,
- * or you may create API specific credentials from within your PayPal Manager account.
- */
-$config['PayFlowUsername'] = $config['Sandbox'] ? 'tester' : '';
-$config['PayFlowPassword'] = $config['Sandbox'] ? 'Pr0t3ct!' : '';
-$config['PayFlowVendor'] = $config['Sandbox'] ? 'angelleye' : '';
-$config['PayFlowPartner'] = $config['Sandbox'] ? 'PayPal' : 'PayPal';
-
-/*
- * PayPal REST API Credentials
- * ------------------------------
- */
-$config['RESTAPIClient_Id'] = $config['Sandbox'] ? 'AbWX5DtTNUferjgDtrQfRQjqZIOnK25idCvaPH-R-VUpJZMHFG83LPU_o3-5d32dpnO2oVydlzIHjBRX' : '';
-$config['RESTAPIClient_Secret'] = $config['Sandbox'] ? 'EBn7ARTYLux062X74_hFl_wKkSJMpq69oaVWTMRWWvH9QNt_JMjJ8G0rGczEB86skZf90goieMOQA0Qu' : '';
-
-/**
- * Set Domain
- */
-$config['Domain'] = ($config['Sandbox']) ? 'http://dev:2600/angelleye/angelleye-paypal-php-virtual-terminal-pos/' : 'http://www.domain.com/paypal-pos/';
 
 /**
  * Enable Sessions
@@ -86,7 +31,72 @@ if($config['Sandbox'])
 }
 
 /**
- * Set Currency sign (default is US Dollar Sign $)
+ * API Selection
+ * -------------
+ * This solution supports all 3 versions of PayPal APIs for credit card processing.
+ *
+ * Available Options: PayPalPro-DDP, PayPalPro-PayFlow, PayPal-Rest
+ *
+ * PayPalPro-DDP - This is used with Website Payments Pro 3.0 (DoDirectPayment API).
+ * PayPalPro-PayFlow - This is used with Payments Pro 1.5 or Payments Pro 2.0 (PayFlow API)
+ * PayPal-Rest - This is used with PayPal's REST API.
+ *
+ * Set this config option to the value shown above that matches what your PayPal account
+ * is configured to use for processing credit cards directly.
+ */
+$config['ApiSelection'] = 'PayPal-Rest';
+
+/**
+ * PayPalPro-DDP API Credentials
+ * -------------------------------------
+ * These are your PayPal API credentials for working with PayPal Website Payments Pro (DoDirectPayment).
+ * We're using shorthand if/else statements here to set both Sandbox and Production values.
+ * Your sandbox values go on the left and your live values go on the right.
+ *
+ * You may obtain these credentials by logging into the following link with your PayPal account.
+ *
+ * Sandbox: https://www.sandbox.paypal.com/us/cgi-bin/webscr?cmd=_get-api-signature&generic-flow=true
+ * Live: https://www.paypal.com/us/cgi-bin/webscr?cmd=_get-api-signature&generic-flow=true
+ */
+$config['APIUsername'] = $config['Sandbox'] ? 'sandbo_1215254764_biz_api1.angelleye.com' : 'LIVE_USERNAME_GOGES_HERE';
+$config['APIPassword'] = $config['Sandbox'] ? '1215254774' : 'LIVE_PASSWORD_GOGES_HERE';
+$config['APISignature'] = $config['Sandbox'] ? 'AiKZhEEPLJjSIccz.2M.tbyW5YFwAb6E3l6my.pY9br1z2qxKx96W18v' : 'LIVE_SIGNATURE_GOGES_HERE';
+
+/**
+ * PayPalPro-Payflow API Credentials
+ * ------------------------------
+ * These are the credentials you use for your PayPal Payments Pro 1.5 / 2.0 account.
+ * These types of accounts use the PayPal Manager:  http://manager.paypal.com
+ *
+ * We're using shorthand if/else statements here to set both Sandbox and Production values.
+ * Your sandbox values go on the left and your live values go on the right.
+ *
+ * You may use the same credentials you use to login to your PayPal Manager,
+ * or you may create API specific credentials from within your PayPal Manager account.
+ *
+ * You do need to include values for all 4 variables here, so if you are using the same
+ * credentials you use to login to the PayPal Manager site then you will need to set
+ * the same value for both PayFlowUsername and PayFlowVendor.
+ */
+$config['PayFlowUsername'] = $config['Sandbox'] ? 'tester' : 'LIVE_USERNAME_GOES_HERE';
+$config['PayFlowPassword'] = $config['Sandbox'] ? 'Pr0t3ct!' : 'LIVE_PASSWORD_GOES_HERE';
+$config['PayFlowVendor'] = $config['Sandbox'] ? 'angelleye' : 'LIVE_VENDOR_GOES_HERE';
+$config['PayFlowPartner'] = $config['Sandbox'] ? 'PayPal' : 'LIVE_PARTNER_GOES_HERE';
+
+/**
+ * PayPal REST API Credentials
+ * ---------------------------
+ * These are the credentials used with PayPal's REST API.
+ * These types of accounts are configured through your PayPal Developer Account (http://developer.paypal.com)
+ *
+ * We're using shorthand if/else statements here to set both Sandbox and Production values.
+ * Your sandbox values go on the left and your live values go on the right.
+ */
+$config['RESTAPIClient_Id'] = $config['Sandbox'] ? 'AbWX5DtTNUferjgDtrQfRQjqZIOnK25idCvaPH-R-VUpJZMHFG83LPU_o3-5d32dpnO2oVydlzIHjBRX' : 'LIVE_CLIENT_ID_GOES_HERE';
+$config['RESTAPIClient_Secret'] = $config['Sandbox'] ? 'EBn7ARTYLux062X74_hFl_wKkSJMpq69oaVWTMRWWvH9QNt_JMjJ8G0rGczEB86skZf90goieMOQA0Qu' : 'LIVE_CLIENT_SECRET_GOES_HERE';
+
+/**
+ * Set Currency Sign (default is US Dollar Sign $)
  */
 $config['CurrencySign'] = '$';
 
@@ -103,11 +113,10 @@ $config['CurrencyCode'] = 'USD';
 $config['ShowNavMenuLeft'] = TRUE;
 
 /**
- * Show swipe field on pos form
+ * Show/hide swipe field on POS form.
  * TRUE == Show, FALSE == HIDE
  */
 $config['ShowSwipeField'] = TRUE;
 
 
 /* End of file config.php */
-/* Location: ./includes/config.php */
